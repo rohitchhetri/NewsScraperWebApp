047fad30edb2f5b8a60c885548a52b7cf7792830
Rohit Chhetri <rohit.chhetri@gmail.com>
2019-02-08 17:49:31 -0800

    update change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3080;
+var port = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
